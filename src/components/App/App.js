8db1a6489374f97df2cfbe2c6c845289dfaaf7ea
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 15:26:18 -0600

    imported NewUser

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Login from '../Login/Login';
+import NewUser from '../NewUser/NewUser';
 import Signup from '../Signup/Signup';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Router>
         <Route exact path='/' component={Login} />
         <Route path='/signup' component={Signup} />
+        <Route path='/welcome' component={NewUser} />
       </Router>
     </>
   );
