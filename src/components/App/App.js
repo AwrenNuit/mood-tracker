982c03aeb15c8811bb3a43e381983426c7c7f236
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 10:49:07 -0600

    imported Signup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Login from '../Login/Login';
+import Signup from '../Signup/Signup';
 
 export default function App() {
   return (
     <>
       <Router>
         <Route exact path='/' component={Login} />
-        <Route path='/signup' component={} />
+        <Route path='/signup' component={Signup} />
       </Router>
     </>
   );
