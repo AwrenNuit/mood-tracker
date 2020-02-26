ec036bac1053fba3444ad21b5218e460ed35b765
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 15:17:11 -0600

    created component, added rough draft text

---
+++ src/components/NewUser/NewUser.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function NewUser() {
+
+  return(
+    <center>
+      <div className="main-container">
+        <h1>Welcome, dearest NAME.</h1>
+        <p>MOOD TRACKER is a place of honesty and non-judgement. This is for your own personal growth, 
+          so no masks are needed here. Take a minute or two to close your eyes, breathe deep, and re-live 
+          a comforting memory - maybe a big hug, a cozy evening by the fireplace, or a time your ideas were 
+          supported. The choice is yours. After taking a moment to ground yourself you're ready to begin.
+        </p>
+        <p>
+          MOOD TRACKER helps you track various aspects of your life - Mood, Food, Movement, Sleep, and Therapy. 
+          Each of these, with the exception of Mood, can be turned on or off at your discretion. Tracking things 
+          like these can be quite beneficial to the health (both physical and mental) of some people. You may 
+          also add a short journal entry each day if you would like.
+        </p>
+        <p>
+          Building new habits can be difficult, and replacing old habits that are no longer serving you can be 
+          an even bigger challenge. However, when things like mood or sleep are tracked patterns can emerge. 
+          Seeing and acknowledging these patterns can be the first step on the path towards being the version 
+          of yourself that you want to be. Your future self with thank you.
+        </p>
+        <p>
+          So how are you feeling about today?
+        </p>
+      </div>
+    </center>
+  );
+}
