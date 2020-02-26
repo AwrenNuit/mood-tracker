7846bb9b1da0392dadf9f70bc9aa9c0681c51f95
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 10:48:22 -0600

    began setting up routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Login from '../Login/Login';
 
 export default function App() {
   return (
     <>
-
+      <Router>
+        <Route exact path='/' component={Login} />
+        <Route path='/signup' component={} />
+      </Router>
     </>
   );
 }
