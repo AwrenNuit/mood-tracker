9fa3386832365ac794d1589cbfbc6b257ae95315
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 11:55:30 -0600

    set up index template for redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware, logger] 
+  :
+  [sagaMiddleware];
+
+const store = createStore(
+  // rootReducer,
+  applyMiddleware(...middlewareList),
+);
+
+// sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
