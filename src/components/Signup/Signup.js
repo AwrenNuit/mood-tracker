53f09272ecf30fa10e82ef499e9d606e81e0c44d
AwrenNuit <a.rennaker@yahoo.com>
2020-02-26 10:56:29 -0600

    created template for signup page

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup() {
 
+  const dispatch = useCallback(useDispatch());
+  const history = useHistory();
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleSignup = () => password === confirmPassword ? dispatch({type: `REGISTER`, payload: email, password, username}) : '';
+
+  const pushToLoginPage = () => history.push('/');
+
   return(
-    <>
-    </>
+    <div className="login-register-main-container">
+      <h1>Sign Up</h1>
+      <div className="login-register-content-container">
+        <form onSubmit={handleSignup}>
+          <input type="text" onChange={setUsername(e.target.value)} />
+          <input type="text" onChange={setEmail(e.target.value)} />
+          <input type="text" onChange={setPassword(e.target.value)} />
+          <input type="text" onChange={setConfirmPassword(e.target.value)} />
+          <button type="submit">Sign Up</button>
+        </form>
+        <hr />
+        <button onClick={pushToLoginPage}>cancel</button>
+      </div>
+    </div>
   );
 }
