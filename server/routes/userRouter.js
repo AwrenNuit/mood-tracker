b5b4885239ff13b6b136490fe7a8426fe40ba871
AwrenNuit <a.rennaker@yahoo.com>
2020-02-27 11:19:30 -0600

    updated login query

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/login', (req, res)=>{
-  const id = [req.body.id, req.body.password];
+  const id = [req.body.password, req.body.username];
   const sqlQuery = `SELECT * FROM user
-                    WHERE id = $1 AND password = $2;`;
+                    WHERE password = $1 AND username = $2;`;
   pool.query(sqlQuery, id)
-  .then(result=>result.rows)
+  .then(result=>result.rows[0])
   .catch(error=>{
     console.log('Error in /login GET', error);
     res.sendStatus(500);
