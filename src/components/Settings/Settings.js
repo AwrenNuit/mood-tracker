308df4f0e3c53e48a1400818e5721b868c9dfc60
AwrenNuit <a.rennaker@yahoo.com>
2020-02-27 10:51:40 -0600

    set up function templates to update user details and to deactivate account

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -8,22 +8,46 @@ export default function Settings() {
   const history = useHistory();
   const [confirmPassword, setConfirmPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
+
+  const deactivateAccount = id => {
+    let popup = window.confirm(`Are you absolutely sure you want to close your account?`);
+    if(popup){
+      dispatch({type: `DELETE_ACCOUNT`, payload: id});
+    }
+  }
+
+  const updatePersonalDetails = id => {
+    if(password){
+      if(password === confirmPassword){
+        dispatch({type: `PUT_USER_PASSWORD`, payload: id, email, password, username});
+      }
+    }
+    else {
+      dispatch({type: `PUT_USER_DETAILS`, payload: id, email, username});
+    }
+  }
 
   return(
     <>
       <h1>Settings</h1>
       <h3>Personal</h3>
+      <form onSubmit={updatePersonalDetails}>
         <input type="text" value={username} onChange={setUsername} placeholder="username" />
         <input type="text" value={email} onChange={setEmail} placeholder="email" />
         <input type="password" value={password} onChange={setPassword} placeholder="password" />
         <input type="password" value={confirmPassword} onChange={setConfirmPassword} placeholder="confirm password" />
+        <button type="submit">Save</button>
+      </form>
       <h3>Trackers</h3>
         <input type="checkbox" /> Food
         <input type="checkbox" /> Movement
         <input type="checkbox" /> Sleep
         <input type="checkbox" /> Therapy
+      <h3>Deactivate Account</h3>
+        <button onClick={deactivateAccount}>Deactivate</button>
     </>
   );
 }
