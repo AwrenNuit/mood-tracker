35891b46094568576132fdc1cff5a695f976846f
AwrenNuit <a.rennaker@yahoo.com>
2020-02-27 11:05:53 -0600

    created saga to handle logout

--- src/redux/sagas/loginSaga.js
+++ src/redux/sagas/loginSaga.js
@@ -3,15 +3,25 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* login(action){
   try{
-    const response = yield axios.get('/api/login')
+    const response = yield axios.get('/api/user/login')
     yield put({type: `SET_USER`, payload: response.data});
   } catch(error){
     alert('Error logging in.');
   }
 }
 
+function* logout(action){
+  try{
+    yield axios.get('/api/user/logout')
+    yield put({type: `CLEAR_ALL`});
+  } catch(error){
+    alert('Error logging out.');
+  }
+}
+
 function* loginSaga() {
   yield takeLatest(`LOGIN`, login);
+  yield takeLatest(`LOGOUT`, logout);
 }
   
 export default loginSaga;
