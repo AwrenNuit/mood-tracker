6eae7ddc472806977ab074783551763bb5a5beec
AwrenNuit <a.rennaker@yahoo.com>
2020-03-06 16:42:54 -0600

    update GET

--- src/redux/sagas/trackerSaga.js
+++ src/redux/sagas/trackerSaga.js
@@ -3,7 +3,7 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* getTracker(action){
   try{
-    const id = action.payload.id;
+    const id = action.payload;
     const response = yield axios.get(`/api/tracker/${id}`);
     yield put({type: `SET_TRACKER`, payload: response.data});
   } catch(error){
