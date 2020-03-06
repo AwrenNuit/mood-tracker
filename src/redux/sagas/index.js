aecbd0b6eb099df32889b3002b58bcd4bad2ee82
AwrenNuit <a.rennaker@yahoo.com>
2020-03-06 16:18:48 -0600

    imported tracker saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,8 +1,10 @@
 import { all } from 'redux-saga/effects';
 import login from './loginSaga';
+import tracker from './trackerSaga';
 
 export default function* rootSaga() {
   yield all([
     login(),
+    tracker(),
   ]);
 }
