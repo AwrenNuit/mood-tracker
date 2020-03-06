f403972a2e5340842335954411e0e1193c9da2cd
AwrenNuit <a.rennaker@yahoo.com>
2020-03-06 16:17:11 -0600

    created GET and PUT sagas

---
+++ src/redux/sagas/trackerSaga.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* getTracker(action){
+  try{
+    const id = action.payload.id;
+    const response = yield axios.get(`/api/tracker/${id}`);
+    yield put({type: `SET_TRACKER`, payload: response.data});
+  } catch(error){
+    console.log('Error getting tracker data.', error);
+  }
+}
+
+function* putTracker(action){
+  try{
+    yield axios.put(`/api/tracker`, {data: action.payload});
+    yield put({type: `GET_TRACKER`});
+  } catch(error){
+    alert('Error updating tracker data.');
+  }
+}
+
+function* trackerSaga() {
+  yield takeLatest(`GET_TRACKER`, getTracker);
+  yield takeLatest(`PUT_TRACKER`, putTracker);
+}
+  
+export default trackerSaga;
