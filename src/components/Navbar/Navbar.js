875052d6d7c91fc4c1071befa67451ca410656db
AwrenNuit <a.rennaker@yahoo.com>
2020-03-06 13:21:17 -0600

    created stylesheet, set up basic navbar structure

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar() {
 
+  const history = useHistory();
+
   return(
     <div className="navbar-container">
-      <div className="tab">
-        <button onclick={history.push()}>Entries</button>
+      <div className="navbar-tab navbar-entries">
+        <button className="navbar-btn" onClick={()=>{history.push('/')}}>Entries</button>
       </div>
-      <div className="tab">
-        <button onclick={history.push('/new-entry')}>New Entry</button>
+      <div className="navbar-tab navbar-new">
+        <button className="navbar-btn" onClick={()=>{history.push('/new-entry')}}>New Entry</button>
       </div>
-      <div className="tab">
-        <button onclick={history.push('/settings')}>Settings</button>
+      <div className="navbar-tab navbar-settings">
+        <button className="navbar-btn" onClick={()=>{history.push('/settings')}}>Settings</button>
       </div>
-      <div className="tab">
-        <button onclick={history.push('/login')}>Logout</button>
+      <div className="navbar-tab navbar-logout">
+        <button className="navbar-btn" onClick={()=>{history.push('/')}}>Logout</button>
       </div>
     </div>
   );
