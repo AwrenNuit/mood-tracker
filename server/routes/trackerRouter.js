ffc7af5833dec4b79e4c91131a739bdf5b845682
AwrenNuit <a.rennaker@yahoo.com>
2020-03-06 16:47:10 -0600

    set up put route

--- server/routes/trackerRouter.js
+++ server/routes/trackerRouter.js
@@ -14,4 +14,18 @@ router.get('/:id', (req, res)=>{
   });
 });
 
+router.put('/', (req, res)=>{
+  console.log('req bdy -----------------------------', req.body);
+  const id = [req.body.id, req.body.food, req.body.movement, req.body.sleep, req.body.therapy];
+  const sqlQuery = `UPDATE tracker
+                    SET food = $2, movement = $3, sleep = $4, therapy = $5
+                    WHERE id = $1;`;
+  pool.query(sqlQuery, id)
+  .then(()=>res.sendStatus(200))
+  .catch(error=>{
+    console.log('Error in / PUT', error);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
