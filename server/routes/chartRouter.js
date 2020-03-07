8bb22a04a111a3e26dcf60eea73f58b2b5dcb290
AwrenNuit <a.rennaker@yahoo.com>
2020-03-07 12:36:32 -0600

    created POST route for new entries

--- server/routes/chartRouter.js
+++ server/routes/chartRouter.js
@@ -14,4 +14,16 @@ router.get('/:id', (req, res)=>{
   });
 });
 
+router.post('/', (req, res)=>{
+  const id = [req.body.id, req.body.mood, req.body.food, req.body.movement, req.body.sleep, req.body.therapy, req.body.thoughts];
+  const sqlQuery = `INSERT INTO chart (user_id, mood, food, movement, sleep, therapy, thoughts)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7);`;
+  pool.query(sqlQuery, id)
+  .then(()=>res.sendStatus(201))
+  .catch(error=>{
+    console.log('Error in / POST', error);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
