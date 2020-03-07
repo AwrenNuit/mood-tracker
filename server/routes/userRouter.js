b6fab52d64cc18aca13f83d6435b5300e6a48345
AwrenNuit <a.rennaker@yahoo.com>
2020-03-07 11:49:16 -0600

    set up PUT routes to update user details

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -14,4 +14,42 @@ router.get('/login/:password/:username', (req, res)=>{
   });
 });
 
+router.get('/:id', (req, res)=>{
+  const id = [req.params.id];
+  const sqlQuery = `SELECT * FROM "user"
+                    WHERE id = $1;`;
+  pool.query(sqlQuery, id)
+  .then(result=>res.send(result.rows[0]))
+  .catch(error=>{
+    console.log('Error in /:id GET', error);
+    res.sendStatus(500);
+  });
+});
+
+router.put('/details', (req, res)=>{
+  const id = [req.body.id, req.body.email];
+  const sqlQuery = `UPDATE "user"
+                    SET email = $2
+                    WHERE id = $1;`;
+  pool.query(sqlQuery, id)
+  .then(()=>res.sendStatus(200))
+  .catch(error=>{
+    console.log('Error in /details PUT', error);
+    res.sendStatus(500);
+  });
+});
+
+router.put('/password', (req, res)=>{
+  const id = [req.body.id, req.body.email, req.body.password];
+  const sqlQuery = `UPDATE "user"
+                    SET email = $2, password = $3
+                    WHERE id = $1;`;
+  pool.query(sqlQuery, id)
+  .then(()=>res.sendStatus(200))
+  .catch(error=>{
+    console.log('Error in /password PUT', error);
+    res.sendStatus(500);
+  });
+});
+
 module.exports = router;
