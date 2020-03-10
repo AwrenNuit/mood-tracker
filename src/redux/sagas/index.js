1c82174e8915be8516769a4e7120afcf782f9be6
AwrenNuit <a.rennaker@yahoo.com>
2020-03-10 10:19:07 -0500

    imported registration saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import chart from './chartSaga';
 import login from './loginSaga';
+import register from './registrationSaga';
 import tracker from './trackerSaga';
 import user from './userSaga';
 
@@ -8,6 +9,7 @@ export default function* rootSaga() {
   yield all([
     chart(),
     login(),
+    register(),
     tracker(),
     user(),
   ]);
