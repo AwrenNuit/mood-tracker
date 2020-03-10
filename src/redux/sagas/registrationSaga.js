1cbca20e48c79bf3745d6fb03453ad74a63a063a
AwrenNuit <a.rennaker@yahoo.com>
2020-03-10 10:21:41 -0500

    minor formatting updates

--- src/redux/sagas/registrationSaga.js
+++ src/redux/sagas/registrationSaga.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 function* registerUser(action) {
   try {
-    yield put({ type: 'CLEAR_REGISTRATION_ERROR' });
-    yield axios.post('/api/user/register', action.payload);
-    yield put({ type: 'LOGIN', payload: {email: action.payload.email, password: action.payload.password} });
-    yield put({type: 'SET_TO_LOGIN_MODE'});
+    yield put({type: `CLEAR_REGISTRATION_ERROR`});
+    yield axios.post(`/api/user/register`, action.payload);
+    yield put({type: `LOGIN`, payload: {email: action.payload.email, password: action.payload.password}});
+    yield put({type: `SET_TO_LOGIN_MODE`});
   } catch (error) {
       console.log('Error signing up.', error);
-      yield put({type: 'REGISTRATION_FAILED'});
+      yield put({type: `REGISTRATION_FAILED`});
   }
 }
 
