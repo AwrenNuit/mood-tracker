ebd5207228088129db89af6b9dbb3553e35f6380
AwrenNuit <a.rennaker@yahoo.com>
2020-03-11 14:33:16 -0500

    added method to handle logging out

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './Navbar.css';
 
 export default function Navbar() {
 
+  const dispatch = useDispatch();
   const history = useHistory();
+
+  const handleLogout = () => {
+    dispatch({type: `LOGOUT`});
+    dispatch({type: `CLEAR_ALL`});
+    history.push(`/login`);
+  }
 
   return(
     <div className="navbar-container">
@@ -18,7 +26,7 @@ export default function Navbar() {
         <button className="navbar-btn" onClick={()=>{history.push('/settings')}}>Settings</button>
       </div>
       <div className="navbar-tab navbar-logout">
-        <button className="navbar-btn" onClick={()=>{history.push('/login')}}>Logout</button>
+        <button className="navbar-btn" onClick={handleLogout}>Logout</button>
       </div>
     </div>
   );
