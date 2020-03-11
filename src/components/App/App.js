e54d1e9488b69ba8ba720d61f624cafa1fc31158
AwrenNuit <a.rennaker@yahoo.com>
2020-03-10 22:10:15 -0500

    set login check, redirects / to either /login or /home

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useLocation, Redirect } from 'react-router-dom';
 import './App.css';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
@@ -11,10 +11,31 @@ import Signup from '../Signup/Signup';
 
 export default function App() {
   
+  const dispatch = useCallback(useDispatch());
   const location = useLocation();
+  const user = useSelector(state => state.user);
+
+  useEffect(() => {
+    dispatch({type: 'FETCH_USER'});
+  }, [dispatch]);
+
+  useEffect(() => {
+    if(user && user.id){
+      dispatch({ type: 'GET_USER_DETAILS', payload: userID.id });
+    }
+  },[dispatch, user]);
 
   return (
     <Router>
+      {user ?
+        <Route exact path='/'>
+          <Redirect to='/home' />
+        </Route>
+      :
+        <Route exact path='/'>
+          <Redirect to='/login' />
+        </Route>
+      }
       <Route exact path='/login' component={Login} />
       <Route path='/home' component={Home} />
       <Route path='/new-entry' component={NewEntry} />
