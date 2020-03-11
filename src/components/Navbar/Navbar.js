36acf0169c3661417a2afaee30308cfe0b0c20f5
AwrenNuit <a.rennaker@yahoo.com>
2020-03-10 21:50:24 -0500

    updated url pathing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
   return(
     <div className="navbar-container">
       <div className="navbar-tab navbar-entries">
-        <button className="navbar-btn" onClick={()=>{history.push('/')}}>Entries</button>
+        <button className="navbar-btn" onClick={()=>{history.push('/home')}}>Entries</button>
       </div>
       <div className="navbar-tab navbar-new">
         <button className="navbar-btn" onClick={()=>{history.push('/new-entry')}}>New Entry</button>
@@ -18,7 +18,7 @@ export default function Navbar() {
         <button className="navbar-btn" onClick={()=>{history.push('/settings')}}>Settings</button>
       </div>
       <div className="navbar-tab navbar-logout">
-        <button className="navbar-btn" onClick={()=>{history.push('/')}}>Logout</button>
+        <button className="navbar-btn" onClick={()=>{history.push('/login')}}>Logout</button>
       </div>
     </div>
   );
