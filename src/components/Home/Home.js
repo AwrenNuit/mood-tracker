504286cea3397da9c48a74e348d892ce38d7d5da
AwrenNuit <a.rennaker@yahoo.com>
2020-03-11 19:56:47 -0500

    removed unnecessary code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,15 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import HomeCalendar from '../HomeCalendar/HomeCalendar';
 
 export default function Home() {
 
   const dispatch = useCallback(useDispatch());
-  const history = useHistory();
   const quote = useSelector(state => state.quote);
-  const [date, setDate] = useState('');
 
   useEffect(()=>{
     dispatch({type: `GET_QUOTE`});
   }, [dispatch]);
-
-  const pushToCharts = () => history.push('/charts');
-
-  const pushToNewEntry = () => {
-    if(date){
-      dispatch({type: `SET_DATE`, payload: date});
-      history.push('/new-entry');
-    }
-  }
-
-  const pushToSettings = () => history.push('/settings');
 
   return(
     <center>
