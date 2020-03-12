07a705803e15619654a40ad4384142043643d8d6
AwrenNuit <a.rennaker@yahoo.com>
2020-03-11 20:15:39 -0500

    updated POST for new database rows

--- server/routes/chartRouter.js
+++ server/routes/chartRouter.js
@@ -15,9 +15,23 @@ router.get('/:id', (req, res)=>{
 });
 
 router.post('/', (req, res)=>{
-  const id = [req.body.id, req.body.mood, req.body.food, req.body.movement, req.body.sleep, req.body.therapy, req.body.thoughts];
-  const sqlQuery = `INSERT INTO chart (user_id, mood, food, movement, sleep, therapy, thoughts)
-                    VALUES ($1, $2, $3, $4, $5, $6, $7);`;
+  const id = [
+    req.body.id, 
+    req.body.challenge, 
+    req.body.date, 
+    req.body.food, 
+    req.body.meds, 
+    req.body.mood, 
+    req.body.movement, 
+    req.body.pain, 
+    req.body.period, 
+    req.body.sleep, 
+    req.body.therapy, 
+    req.body.thoughts, 
+    req.body.water
+  ];
+  const sqlQuery = `INSERT INTO chart (challenge, date, food, meds, mood, movement, pain, period, sleep, therapy, thoughts, water, user_id)
+                    VALUES ($2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $1);`;
   pool.query(sqlQuery, id)
   .then(()=>res.sendStatus(201))
   .catch(error=>{
