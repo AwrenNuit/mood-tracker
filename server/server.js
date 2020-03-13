d79739894ba61eb158849757e0fa6767d1140456
AwrenNuit <a.rennaker@yahoo.com>
2020-03-13 15:30:34 -0500

    required in passport and other middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user-strategy');
 const PORT = process.env.PORT || 5000;
 const chartRoute = require('./routes/chartRouter');
 const quoteRoute = require('./routes/quoteRouter');
@@ -10,6 +12,9 @@ const userRoute = require('./routes/userRouter');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api/chart', chartRoute);
 app.use('/api/quote', quoteRoute);
