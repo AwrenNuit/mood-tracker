2e565ebaed2e4ebe92d0bf28da18f793fecc1fe8
AwrenNuit <a.rennaker@yahoo.com>
2020-05-24 13:24:27 -0500

    replaced switch case with array of months

--- src/components/HomeCalendar/HomeCalendar.js
+++ src/components/HomeCalendar/HomeCalendar.js
@@ -1,52 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 export default function HomeCalendar(props) {
 
-  const [month, setMonth] = useState('');
+  const monthList = [
+    "January", "February", "March", "April", "May", "June", "July", 
+    "August", "September", "October", "November", "December"
+  ];
+  const [month, setMonth] = useState(monthList[props.month]);
   const [year, setYear] = useState(props.year);
-
-  useEffect(() => {
-    switch(props.month){
-      case 0:
-        setMonth('January');
-        break;
-      case 1:
-        setMonth('February');
-        break;
-      case 2:
-        setMonth('March');
-        break;
-      case 3:
-        setMonth('April');
-        break;
-      case 4:
-        setMonth('May');
-        break;
-      case 5:
-        setMonth('June');
-        break;
-      case 6:
-        setMonth('July');
-        break;
-      case 7:
-        setMonth('August');
-        break;
-      case 8:
-        setMonth('September');
-        break;
-      case 9:
-        setMonth('October');
-        break;
-      case 10:
-        setMonth('November');
-        break;
-      case 11:
-        setMonth('December');
-        break;
-      default:
-        break;
-    }
-  }, []);
 
   return(
     <center>
