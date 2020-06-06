6b283b339a3c20732edfd067ce591741dc3932b7
AwrenNuit <a.rennaker@yahoo.com>
2020-06-06 16:12:53 -0500

    clarified query

--- server/routes/calendarRouter.js
+++ server/routes/calendarRouter.js
@@ -3,12 +3,11 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/:id', (req, res)=>{
-  console.log('id:', req.params.id);
   const id = [req.params.id];
   const sqlQuery = `SELECT date, mood FROM chart
-                    WHERE id = $1;`;
+                    WHERE user_id = $1;`;
   pool.query(sqlQuery, id)
-  .then(result=>res.send(result.rows[0]))
+  .then(result=>res.send(result.rows))
   .catch(error=>{
     console.log('Error in /:id GET', error);
     res.sendStatus(500);
